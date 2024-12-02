3c1ab20b4193183952461bb0560d81ea58be4717
Haris Pathan <156300458+haristrilokn@users.noreply.github.com>
2024-12-02 11:04:31 +0530

    Function of calculating final checksum, verifying integrity

--- lib/view_model/google_ml_kit_viewmodel.dart
+++ lib/view_model/google_ml_kit_viewmodel.dart
@@ -5,13 +5,18 @@ import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart
 import 'package:intl/intl.dart';
 
 class GoogleMlKitViewmodel extends ChangeNotifier {
-  String license = "";
   String firstName = "";
   String lastName = "";
   String documentNumber = "";
   String gender = "";
   String dob = "";
   String doe = "";
+  String? extraData;
+  int documentNumberChecksum = 0,
+      dobChecksum = 0,
+      doeChecksum = 0,
+      finalChecksum = 0;
+  int? extraDataChecksum;
   bool isVerified = false;
   List<String> mrz = [];
   TextRecognizer textRecognizer =
@@ -31,10 +36,10 @@ class GoogleMlKitViewmodel extends ChangeNotifier {
     }
     var firstMRZ = mrz.first.replaceAll(" ", "");
     var secondMRZ = mrz.last.replaceAll(" ", "");
-    extractDataOfFirstLine(firstMRZ);
-    extractDataOfSecondLine(secondMRZ);
     log('First MRZ: $firstMRZ');
     log('Second MRZ: $secondMRZ');
+    extractDataOfFirstLine(firstMRZ);
+    extractDataOfSecondLine(secondMRZ);
   }
 
   void extractDataOfFirstLine(String text) {
@@ -43,15 +48,55 @@ class GoogleMlKitViewmodel extends ChangeNotifier {
     lastName = text.substring(0, indexOfFirstSign);
     text = text.replaceFirst(lastName, "");
     text = text.replaceAll("<", " ");
+    text = text.replaceAll("«", " ");
     firstName = text;
     firstName = text.trim();
   }
 
   void extractDataOfSecondLine(String text) {
+    text = text.replaceAll("«", "<");
+    text = text.replaceAll(" ", "");
     documentNumber = text.substring(0, 9);
-    gender = text[20];
+    documentNumberChecksum = int.parse(text[9]);
     dob = text.substring(13, 19);
+    dobChecksum = int.parse(text[19]);
+    gender = text[20];
     doe = text.substring(21, 27);
+    doeChecksum = int.parse(text[27]);
+    if (text[28] != "<") {
+      int lastIndex = text.lastIndexOf("<");
+      if (lastIndex == -1) {
+        lastIndex = 42;
+      }
+      extraData = text.substring(28, lastIndex);
+      extraDataChecksum = int.parse(text[lastIndex + 1]);
+    }
+    finalChecksum = int.parse(text[text.length - 1]);
+    String extraDataString =
+        extraData != null ? (extraData! + extraDataChecksum.toString()) : "";
+
+    //Verify Authenticity
+    int passportResult = calculateChecksum(documentNumber);
+    int dobResult = calculateChecksum(dob);
+    int doeResult = calculateChecksum(doe);
+    int finalResult = calculateChecksum(documentNumber.replaceAll("<", "") +
+        documentNumberChecksum.toString() +
+        dob +
+        dobChecksum.toString() +
+        doe +
+        doeChecksum.toString() +
+        extraDataString);
+    log("final result: $finalResult");
+    if (passportResult == documentNumberChecksum &&
+        dobResult == dobChecksum &&
+        doeResult == doeChecksum &&
+        finalResult == finalChecksum) {
+      isVerified = true;
+    }
+    log("passportResult: ${passportResult == documentNumberChecksum}");
+    log("dobResult: ${dobResult == dobChecksum}");
+    log("doeResult: ${doeResult == doeChecksum}");
+    log("finalChecksum: ${finalResult == finalChecksum}");
     notifyListeners();
   }
 
@@ -79,6 +124,27 @@ class GoogleMlKitViewmodel extends ChangeNotifier {
     final DateFormat formatter = DateFormat('dd MMMM yyyy');
     final String formatted = formatter.format(dateTime);
     return formatted;
+  }
+
+  int calculateChecksum(String input) {
+    final weights = [7, 3, 1];
+    int checksum = 0;
+
+    for (int i = 0; i < input.length; i++) {
+      final char = input[i];
+      int value;
+      if (RegExp(r'[0-9]').hasMatch(char)) {
+        value = int.parse(char); // Numeric value for digits
+      } else if (RegExp(r'[A-Z]').hasMatch(char)) {
+        value = char.codeUnitAt(0) - 'A'.codeUnitAt(0) + 10;
+      } else if (char == '<') {
+        value = 0;
+      } else {
+        throw Exception("Invalid character in MRZ: $char");
+      }
+      checksum += value * weights[i % weights.length];
+    }
+    return checksum % 10;
   }
 
   void clearData() {
